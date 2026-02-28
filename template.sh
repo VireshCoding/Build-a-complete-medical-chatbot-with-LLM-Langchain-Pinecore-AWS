0e5ee23b7087421d412dd934ce5a23e4de5562c2
Viresh Devgan <vireshdevgan@Vireshs-MacBook-Pro.local>
2026-02-28 18:35:45 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Create a directory for the project
+mkdir -p src
+mkdir -p research
+
+# creating file
+touch src/__init__.py
+touch src/header.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!"
+
